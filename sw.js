cde604b089699f428a80945ef06cbf26c3ee7f02
joelduprey <joelduprey@yahoo.com>
2025-03-26 22:22:06 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-const CACHE_NAME = 'racing-schedule-cache-v1';
+const CACHE_NAME = 'racing-schedule-pwa-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  '/racing-schedule-pwa/',
+  '/racing-schedule-pwa/index.html',
+  '/racing-schedule-pwa/manifest.json',
+  '/racing-schedule-pwa/192x192.png',
+  '/racing-schedule-pwa/512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Caching files:', urlsToCache);
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('Cache addAll failed:', err))
   );
 });
 
@@ -18,5 +22,22 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+      .catch(err => console.error('Fetch failed:', err))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
